a3998334e44cae7552c5c7d17e8866f9d47fcade
Bayu <Bayu.Budianto@nttindonesia.com>
2022-07-31 00:12:43 +0700

    Add project files.

---
+++ LonelyInteger/Program.cs
@@ -0,0 +1,9 @@
+﻿List<int> a = new List<int> { 1, 2, 3, 4, 3, 2, 1 };
+foreach (var item in a)
+{
+    var result = a.Select(x => x).Where(w => ((uint)w) == item);
+    if (result.Count() == 1)
+        Console.WriteLine($"{item}");
+}
+
+Console.ReadKey();
